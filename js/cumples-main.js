85560e0e43476096f5001ca632c5b615dae94c06
Quintana <mquintanasolutions@gmail.com>
2026-09-23 18:43:25 +0200

    fix: imports correctos en cada *-main.js y visor PDF de cumples

--- js/cumples-main.js
+++ js/cumples-main.js
@@ -2,7 +2,7 @@ import { initAnalytics } from './analytics.js';
 import { initCookieBanner } from './cookie-banner.js';
 import { initNavbar } from './navbar.js';
 import { initAssistant } from './assistant.js';
-import { initCumples, initCumplesPdf } from './cumples.js';
+import { initCumples } from './cumples.js';
 import { initFooterHours } from './footer.js';
 import { initBackToTop } from './back-to-top.js';
 
@@ -11,6 +11,5 @@ initCookieBanner();
 initNavbar();
 initAssistant();
 initCumples();
-initCumplesPdf();
 initFooterHours();
 initBackToTop();
